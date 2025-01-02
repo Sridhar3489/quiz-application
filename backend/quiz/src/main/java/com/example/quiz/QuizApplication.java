d9c2f2f60eca3ffdcadee2eda129c5b4b3ca8d89
Sridhar3489 <saisridharparimi@gmail.com>
2025-01-02 17:27:43 -0500

    Backend Changes

--- backend/quiz/src/main/java/com/example/quiz/QuizApplication.java
+++ backend/quiz/src/main/java/com/example/quiz/QuizApplication.java
@@ -2,8 +2,12 @@ package com.example.quiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan("com.example.quiz.model")
+@ComponentScan("com.example.quiz")
 public class QuizApplication {
 
 	public static void main(String[] args) {
