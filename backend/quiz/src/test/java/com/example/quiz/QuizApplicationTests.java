ac26361421bbd74a16d39729a5a44bffadb7cceb
Sridhar3489 <saisridharparimi@gmail.com>
2025-01-02 15:35:41 -0500

    pom changes

--- backend/quiz/src/test/java/com/example/quiz/QuizApplicationTests.java
+++ backend/quiz/src/test/java/com/example/quiz/QuizApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.quiz;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = QuizApplication.class)
 class QuizApplicationTests {
 
 	@Test
